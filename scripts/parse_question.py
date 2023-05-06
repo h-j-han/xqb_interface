b7dc9c78166448c6c7ab043409a28de79a7eef47
h-j-han <19178967+h-j-han@users.noreply.github.com>
2023-05-06 20:52:23 +0000

    Update from the last year and recent years

--- scripts/parse_question.py
+++ scripts/parse_question.py
@@ -6,7 +6,8 @@ from sqlalchemy import create_engine
 import sqlalchemy
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
 
-file_path = os.path.join(os.getenv("MYHOME"), 'gits/xqb_interface/data/df4sql_xqb_initial_question.pickle')
+# file_path = os.path.join(os.getenv("MYHOME"), 'gits/xqb_interface/data/df4sql_xqb_initial_question.pickle')
+file_path = os.path.join(os.getenv("MYHOME"), 'gits/xqb_interface/data/df4sql_xqb_feetthinking_question.pickle')
 with open(file_path, 'rb') as f:
     df = pickle.load(f)
 df['translations'] = list(map(lambda x: json.dumps(x), df['translations']))
